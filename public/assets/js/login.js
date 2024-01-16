1131c17a3a3f94a82350403a3b04f1ad64879528
Huto <92351427+Hutotpn@users.noreply.github.com>
2024-01-16 07:55:16 +0000

    Dashboard

---
+++ public/assets/js/login.js
@@ -0,0 +1,37 @@
+document
+  .getElementById("loginForm")
+  .addEventListener("submit", async function (event) {
+    event.preventDefault();
+
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+
+    try {
+      const response = await fetch("/.netlify/functions/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ username, password }),
+      });
+
+      if (!response.ok) {
+        if (response.status === 401) {
+          // Handle 401 Unauthorized (authentication failure)
+          alert("Authentication failed: Invalid username or password");
+        }
+        if (response.status === 500) {
+          // Handle 500 Internal Server Error (other server error)
+          alert("Login failed: Internal server error");
+        } else {
+          const errorData = await response.json();
+          throw new Error(errorData.error || "Login failed");
+        }
+      }
+
+      location.href = "./dashboard";
+    } catch (error) {
+      console.error("Login error:", error.message);
+      alert("Login failed: " + error.message);
+    }
+  });
